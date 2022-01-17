0d7e94adbebc197eb5e215a0c586d51ddbb7c840
sdsso <sdsso@DESKTOP-IMS0U25>
2022-01-18 00:55:58 +0900

    20220117

--- src/Answer1/AplusB.java
+++ src/Answer1/AplusB.java
@@ -1,10 +1,14 @@
 package Answer1;
 
+import java.util.Scanner;
+
 public class AplusB {
 
 	public static void main(String[] args) {
-		System.out.println();
-		System.out.println();
+		Scanner input = new Scanner(System.in);
+		int a = input.nextInt();
+		int b = input.nextInt();
+		System.out.println(a+b);
 	}
 
 }
