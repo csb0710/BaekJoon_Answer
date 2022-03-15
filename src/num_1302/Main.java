a9e7c5b30a6e024cc22d4405d2c91f88043568a2
sdsso <sdsso@115.23.140.122>
2022-03-15 20:36:57 +0900

    solve 1302

--- src/num_1302/Main.java
+++ src/num_1302/Main.java
@@ -1,8 +1,5 @@
 package num_1302;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -11,57 +8,46 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args){
 		// Answer of BackJoon no.1302
 		
-		ArrayList<String> list = new ArrayList<>();
+		HashMap<String, Integer> list = new HashMap<>();
 		Scanner scan = new Scanner(System.in);
 		int count = scan.nextInt();
-		ArrayList<String> temp = new ArrayList<>();
+		ArrayList<String> sort_list = new ArrayList<>();
 		scan.nextLine();
 		int max = 0;
 		
-		
-		
 		for(int i = 0; i < count; i++) {
 			String input = scan.nextLine();
-			temp.add(input);
-			if(count == 1) {
-				list.add(input);
+			if(list.containsKey(input)) {
+				list.put(input, list.get(input)+1);
+				int temp = list.get(input);
+				if(max < temp) {
+					max = temp;
+					sort_list.clear();
+					sort_list.add(input);
+				}
+				else if(max == temp) {
+					sort_list.add(input);
+				}
+			}
+			else {
+				list.put(input, 1);
+				int temp = list.get(input);
+				if(max < temp) {
+					max = list.get(input);
+					sort_list.add(input);
+				}
+				else if(max == temp) {
+					sort_list.add(input);
+				}
 			}
 		}
 		
-		Collections.sort(temp);
-		String standard = temp.get(0);
-		int counter = 1;
-	
-		for(int i = 1; i < temp.size(); i++) {
-			if(!temp.get(i).equals(standard)) {
-				if(max < counter) {
-					max = counter;
-					counter = 1;
-					list.clear();
-					list.add(standard);
-				}
-				else if(max == counter) {
-					list.add(standard);
-					counter = 1;
-				}
-				standard = temp.get(i);
-			}
-			else 
-				counter ++;
-		}
-		if(max < counter) {
-			list.clear();
-			list.add(standard);
-		}
-		else if(max == counter) {
-			list.add(standard);
-		}
-			
-		Collections.sort(list);
-		System.out.println(list.get(0));
+		Collections.sort(sort_list);
+		
+		System.out.println(sort_list.get(0));
 
 	}
 
