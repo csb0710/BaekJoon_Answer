687f2ee8e7256f5318844a07305b70d3975c16ad
csb0710 <sdssond@gmail.com>
2022-07-14 01:31:18 +0900

    2022/07/14 BaekJoon

--- src/num_2631/Main.java
+++ src/num_2631/Main.java
@@ -1,7 +1,42 @@
 package num_2631;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(br.readLine());
+        ArrayList<Integer> list = new ArrayList<>();
+        int[] input = new int[count];
+        int[] dp = new int[count];
+
+        list.add(-1);
+
+        for(int i = 0; i < count; i++) {
+            input[i] = Integer.parseInt(br.readLine());
+        }
+
+        for(int i = 0; i < count; i++) {
+            dp[i] = 1;
+        }
+
+        int max = 1;
+
+        for(int i = 0; i < count; i++) {
+            for(int j = 0; j < i; j++) {
+                if(input[i] > input[j] && dp[j] > dp[i] - 1) {
+                    dp[i] = dp[j]+1;
+                }
+            }
+            if(max < dp[i]) {
+                max = dp[i];
+            }
+        }
+
+        System.out.println(count-max);
+
     }
 }
